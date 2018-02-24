dac319edec33da7c4e3fcf696047713a8fdb362c
vpranathi17 <33180562+vpranathi17@users.noreply.github.com>
2018-02-24 15:21:04 -0500

    Add files via upload

--- decoder.v
+++ decoder.v
@@ -32,66 +32,7 @@ half h7 (PP[6],cout[6],sum[6],cout[5]);
 half h8 (PP[7],cout[7],sum[7],cout[6]);
 
 initial
-#700 $display ("sum:%b,cout: %b", sum,cout);
+#700 $display ("%b",PP);
 
 endmodule
 
-
-
-
-// module decoder (sdn,y,PP);
-// input [2:0]sdn;
-// input [7:0]y;
-// output [8:0]PP;
-// wire [8:0]p;
-// wire [7:0]o1,o2,pp1;
-// wire s1,s2;
-// genvar j;
-
-// assign p[0]=1'b0;
-// assign p[8:1]=y[7:0];
-
-// assign s1 = ~ sdn[2];
-// assign s2 = ~ sdn[1];
-
-// for (j=1; j<=8;j=j+1)
-// begin
-// assign o1[j-1] = (s1 & sdn[1] & p[j-1]);
-// assign o2[j-1]= (s2 & p[j] & sdn[2]);
-// assign pp1[j-1] = (o1[j-1] | o2[j-1]);
-// assign PP[j-1] = pp1[j-1] ^ sdn[0];
-// end
-
-// assign PP[8] = y[7];
-
-// endmodule
-// module decoder (sdn,y,PP);
-// input [2:0]sdn;
-// input signed [7:0]y;
-// output signed [7:0]PP;
-// wire [7:0]cout;
-// wire cin,temp;
-// wire [7:0]o1,o2,pp1,sum;
-// wire s1,s2;
-// genvar j,k;
- 
-// assign s1 = ~ sdn[2];
-// assign s2 = ~ sdn[1];
-// assign cin = sdn[0];
- 
-// // generate
-// for (j=1; j<=8;j=j+1)
-// begin
-// assign o1[j-1] = (s1 & sdn[1] & y[j-1]);
-// assign o2[j-1]= (s2 & y[j] & sdn[2]);
-// assign pp1[j-1] = (o1[j-1] | o2[j-1]);
-// assign sum[j-1] = pp1[j-1] ^ sdn[0];
-// half h1 (PP[j-1],cout[j-1],sum[j-1],cin);
-// // assign PP[j-1] = sum[j-1] ^ cin;
-// assign cout = sum[j-1] & cin;
-// end
-// // endgenerate
-
-// initial
-// #700 $display ("sum:%b,cout: %b", sum,cout);
-// endmodule
--- encoder.v
+++ encoder.v
@@ -1,17 +1,16 @@
-module encoder(x,y,sdn1,sdn2,sdn3);
+module encoder(x,sdn1,sdn2,sdn3,sdn4);
 
-input [7:0]x,y;
-output [2:0]sdn1, sdn2, sdn3; //single,double,negate
-output [8:0] PP1,PP2,PP3;
+input signed [7:0]x;
+output [2:0]sdn1, sdn2, sdn3, sdn4; //single,double,negate
+// output [8:0] PP1,PP2,PP3;
 
-wire [2:0]xh,xm,xl;
+wire [2:0]xh,xm,xl,xxh;
 
-assign x[5:0]= 6'b111010;
-assign y[5:0]=6'b001101;
 assign xl[0]=1'b0;
 assign xl[2:1]=x[1:0];
 assign xm[2:0]=x[3:1];
 assign xh[2:0]=x[5:3]; 
+assign xxh[2:0]=x[7:5];
 
 function [2:0]encoding;
 input [2:0]a;
@@ -37,9 +36,10 @@ endfunction
 assign sdn1[2:0]= encoding(xl);
 assign sdn2[2:0]= encoding(xm);
 assign sdn3[2:0]= encoding(xh);
+assign sdn4[2:0]= encoding(xxh);
 
-decoder d1 (sdn1,y,PP1);
-decoder d2 (sdn2,y,PP2);
-decoder d3 (sdn3,y,PP3);
+// decoder d1 (sdn1,y,PP1);
+// decoder d2 (sdn2,y,PP2);
+// decoder d3 (sdn3,y,PP3);
 
 endmodule
