827e409b15a4bdc48ee0e8345f8eafe2b90fb618
vpranathi17 <33180562+vpranathi17@users.noreply.github.com>
2018-02-25 18:12:57 -0500

    Update divider.v

--- divider.v
+++ divider.v
@@ -9,11 +9,14 @@ module divider(A,B,Q);
 // 	wire signed [7:0] xint,xjnew,select,fact;
 
 	assign cin =1'b0;
-	
+// for log2n steps	
 	multiplier m1 (A,xi,Ni);
 	multiplier m2 (B,xi,Di);
 	complement c1 (Di[7:0],D);
 	cla ca1 (8'b00000010,D,cin,Fi,cout);
 	multiplier m3 (Fi,xi,Q);
+	
+	assign A = Ni[7:0];
+	assign B = Di[7:0];
 
 endmodule
