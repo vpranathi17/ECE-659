7fbfa5f78ba148325e1c1785d17bc063c1e083c3
vpranathi17 <33180562+vpranathi17@users.noreply.github.com>
2018-02-26 16:09:35 -0500

    Update divider.v

--- divider.v
+++ divider.v
@@ -21,41 +21,53 @@ module divider(A,B,Q);
 function [7:0]out;
 input [7:0] in;
 reg [7:0] temp1;
-integer j;
+integer j,k;
 begin
-assign out[7:0] = in[7:0];
+out[0] = in[0];
 assign temp1[7:0] = in[7:0];
 // assign out [7:1] = ~ in[7:1];
 for(j=1; j<=7; j=j+1)
 begin
 // always @ (temp1[i]) begin
-if (temp1[j-1]== 1'b1) begin
-out[7:j] =  ~ temp1[7:j];
+if (temp1[j]== 1'b1) begin
+out[j] = temp1[j];
+for (k=j+1; k<=7; k=k+1) begin
+out[k] = ~temp1[k]; end
 end
-else if(temp1[j-1] == 1'b0 && (out[j] == 1'b1 || out [j] == 1'b0))
-out[7:j] =  ~ temp1[7:j];
+else if(temp1[j] == 1'b0 && (out[j] == 1'b1 || out [j] == 1'b0)) begin
+for (k=j; k<=7; k=k+1) begin
+out[k] =  ~ temp1[k]; end
+end
 else
 out [j] = temp1[j]; 
 end
 end
 endfunction
 
-// 	for (i=1; i<=3; i=i+1)
-// 	begin	
+function [15:0]fun;
+input 
+integer m;
+endfunction
+
+	for (i=1; i<=3; i=i+1)
+	begin	
 	multiplier m2 (F,xi,Di);
 	assign G = Di[7:0];	
 	multiplier m1 (E,xi,Ni);
+	assign Q = fun(Ni);
 	assign D = out(G);
+// 	complement co (G,Q[7:0]);
 	cla ca1 (8'b00000010,D,cin,Fi,cout);
 	multiplier m3 (Fi,xi,xinew);
-// 	assign Q = Ni;
+	initial
+	# 90 $display("%b",Q);
 	
-// 	assign E = Ni[7:0];
-// 	assign F = Di[7:0];
-// 	assign xi = xinew[7:0];
-// 	end
-	
-	assign Q = D;
+	assign E = Ni[7:0];
+	assign F = Di[7:0];
+	assign xi = xinew[7:0];
+	end
+// 	
+// 	assign Q = Ni ;
 	
 endmodule
 
