29f848d7ddee7a410e754d8775f45011fcf61511
vpranathi17 <33180562+vpranathi17@users.noreply.github.com>
2018-02-27 00:43:25 -0500

    Update divider.v

--- divider.v
+++ divider.v
@@ -1,22 +1,19 @@
-module divider(A,B,Q);
+module divider(A,B,xi,Ni,Di,xinew);
 
-	input signed [7:0] A, B;
-	wire signed [7:0] xi; 
-	output signed [15:0] Q;
-	wire signed [15:0] Ni, Di;
-	wire [7:0]D;
-	wire [7:0]Fi,E,F,G;
+	input signed [7:0] A, B,xi;
+// 	output signed [15:0] xj,E,F;
+	output signed [15:0] Ni, Di, xinew;
+	wire [7:0]D,G,Fi;
 	wire cin,cout;
-	wire [15:0]xinew;
-	reg s;
+// 	wire [15:0]xinew;
 	genvar i;
 	
 // 	wire signed [7:0] xint,xjnew,select,fact;
 
 	assign cin = 1'b0;
-	assign xi = 8'b00000001;
-	assign E = A;
-	assign F = B;
+// 	assign xi = 8'b00000001;
+// 	assign E = A;
+// 	assign F = B;
 // 	s = $clog2($size(A));
 
 function [7:0]out;
@@ -44,39 +41,39 @@ out [j] = temp1[j];
 end
 end
 endfunction
+// 
+// function [15:0]fun;
+// input [15:0]in;
+// integer m;
+// begin
+// for(m=0;m<=15;m=m+1)
+// fun[m] = in[m];
+// end
+// endfunction
 
-function [15:0]fun;
-input [15:0]in;
-integer m;
-begin
-for(m=0;m<=15;m=m+1)
-fun[m] = in[m];
-end
-endfunction
-
-	for (i=1; i<=3; i=i+1)
+// 	for (i=1; i<=3; i=i+1)
 
 // 	while (i<=3)
-	begin	
-	multiplier m2 (F,xi,Di);
+// 	begin	
+	multiplier m2 (B,xi,Di);
 	assign G = Di[7:0];	
-	multiplier m1 (E,xi,Ni);
-	assign Q = fun(Ni);
+	multiplier m1 (A,xi,Ni);
+// 	assign Q = fun(Ni);
 	assign D = out(G);
 // 	complement co (G,D);
 	cla ca1 (8'b00000010,D,cin,Fi,cout);
 	multiplier m3 (Fi,xi,xinew);
-	initial
-	# 90 $display("%b",D);
-	
-	assign E = Ni[7:0];
-	assign F = Di[7:0];
-	assign xi = xinew[7:0];
-	
-	end
+// 	initial
+// 	# 90 $display("%b",Di);
+// 	
+// 	assign E = Ni;
+// 	assign F = Di;
+// 	assign xj = xinew;
+// 	
+// 	end
 	
 // 	assign G= D;
-// 	assign Q = Ni ;
+// 	assign Q = Ni[7:0] ;
 	
 endmodule
 
